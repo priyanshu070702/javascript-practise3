f080a4d5c15b3f72e7e0cd7c0f37831f4677919b
PRIYANSHU DHAPODKAR <priyanshu.dhapodkar2020@vitbhopal.ac.in>
2022-06-18 00:41:20 +0530

    --final

---
+++ practise/index.js
@@ -0,0 +1,6 @@
+heading=document.firstElementChild.lastElementChild.firstElementChild;
+heading.innerHTML = "Good Bye";
+
+document.querySelector("input");
+document.getElementsByClassName("btn")[0].style.color="yellow";
+document.getElementById("title").innerHTML="failo";
